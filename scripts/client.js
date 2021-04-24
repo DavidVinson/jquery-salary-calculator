afa32c3c99dff56267c5a1557be30bd2fb7eb1b3
David Vinson <davidvinson018@gmail.com>
2021-04-24 00:23:23 -0500

    working on no input styling

--- scripts/client.js
+++ scripts/client.js
@@ -1,25 +1,39 @@
 
-employees = [];
+let employees = [];
 
 function onReady() {
     console.log('DOM is ready');
-    $('#add-employee-btnID').click(addEmployee);
-    
-}
+    $('#add-employee-btnID').click(validateEmployeeInfo);
 
-function addEmployee(event) {
-    let employee = {
-        fName: $('#fNameID').val().toLowerCase(),
-        lName: $('#lNameID').val().toLowerCase(),
-        employeeID: $('#employeeID').val(),
-        title: $('#titleID').val(),
-        annualSalary: $('#annualSalaryID').val()
+    // $('#add-employee-btnID').click(addEmployee);
+    
+} //end onReady
+
+function validateEmployeeInfo(event) {
+    let fName = $('#fNameID').val().toLowerCase();
+    let lName = $('#lNameID').val().toLowerCase();
+    let employeeID = $('#employeeID').val();
+    let title = $('#titleID').val();
+    let annualSalary = $('#annualSalaryID').val();
+
+    if (!fName || !lName || !employeeID || !title || !annualSalary) {
+        console.log('need more info');
+        $('.container-form input').css('border', '2px red solid');
+        $('#add-employee-btnID').css({'background-color': 'red', 'color': 'white'});
+
+        clearFields();
     }
-    employees.push(employee);
-    console.log(employee);
+    else {
+        $('#add-employee-btnID').css('-webkit-appearance', 'button');
+        addEmployee(fName, lName, employeeID, title, annualSalary);
+        $('.container-form input').css();
+
+    }
+
     clearFields();
     return;
-} //end addEmployee
+
+}
 
 function clearFields() {
     //clear the DOM fields
@@ -30,6 +44,20 @@ function clearFields() {
     $('#annualSalaryID').val('');
     return;
   } // end clearFields
+
+function addEmployee(fName, lName, employeeID, title, annualSalary) {
+    const employee = {
+        fName,
+        lName,
+        employeeID,
+        title,
+        annualSalary
+    }
+    employees.push(employee);
+    console.log(`Employee Name: ${employee.fName}`);
+    clearFields();
+    return;
+} //end addEmployee
 
 // DOM listen function
 $(onReady);
